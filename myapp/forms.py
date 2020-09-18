294b8e3bff2e76a0f15128e68b881d46b58b8bed
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-09-18 13:42:09 +0530

    first commit

---
+++ myapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from myapp.models import *
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model=Topic
+        fields='__all__'
+
+class WebpageForm(forms.ModelForm):
+    class Meta:
+        model=Webpage
+        fields=('topic','name','url')#'__all__'
